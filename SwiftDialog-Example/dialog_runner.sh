e359417703c93240560b84edd23889ddfa1a9e4f
Gil Burns <gil@gilburns.com>
2024-12-24 13:09:29 -0600

    Update dialog_runner.sh

--- SwiftDialog-Example/dialog_runner.sh
+++ SwiftDialog-Example/dialog_runner.sh
@@ -7,6 +7,7 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
+# remove the alwaysreturninput argument if you only want to see output when button1 is clicked.
 "/usr/local/bin/dialog" --alwaysreturninput --jsonstring "$jsonString" --json 2>/dev/null
 
 response="$?"
